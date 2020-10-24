5931a80829064b745b200d3ca24aad2e61e768fa
Akifumi Fujita <fujita.akifumi@gmail.com>
2020-10-24 11:38:52 +0900

    add branch insns

--- emu.js
+++ emu.js
@@ -10,6 +10,12 @@ const IntRegNames = [
 //
 // Ops
 //
+function UnknownOp() {
+    this.toString = function() {
+        return "UnknownOp";
+    }
+}
+
 function LUI(rd, imm) {
     this.rd = rd;
     this.imm = imm;
@@ -51,9 +57,81 @@ function JALR(rd, rs1, imm) {
     }
 }
 
-function UnknownOp() {
+function BEQ(rs1, rs2, imm) {
+    this.rs1 = rs1;
+    this.rs2 = rs2;
+    this.imm = imm;
     this.toString = function() {
-        return "UnknownOp";
+        if (rs1 == 0) {
+            return `beqz ${IntRegNames[rs2]}, #${imm}`;
+        } else if (rs2 == 0) {
+            return `beqz ${IntRegNames[rs1]}, #${imm}`;
+        } else {
+            return `beq ${IntRegNames[rs1]},${IntRegNames[rs2]},#${imm}`;
+        }
+    }
+}
+
+function BNE(rs1, rs2, imm) {
+    this.rs1 = rs1;
+    this.rs2 = rs2;
+    this.imm = imm;
+    this.toString = function() {
+        if (rs1 == 0) {
+            return `bnez ${IntRegNames[rs2]}, #${imm}`;
+        } else if (rs2 == 0) {
+            return `bnez ${IntRegNames[rs1]}, #${imm}`;
+        } else {
+            return `bne ${IntRegNames[rs1]},${IntRegNames[rs2]},#${imm}`;
+        }
+    }
+}
+
+function BLT(rs1, rs2, imm) {
+    this.rs1 = rs1;
+    this.rs2 = rs2;
+    this.imm = imm;
+    this.toString = function() {
+        if (rs1 == 0) {
+            return `bltz ${IntRegNames[rs2]}, #${imm}`;
+        } else if (rs2 == 0) {
+            return `bltz ${IntRegNames[rs1]}, #${imm}`;
+        } else {
+            return `blt ${IntRegNames[rs1]},${IntRegNames[rs2]},#${imm}`;
+        }
+    }
+}
+
+function BGE(rs1, rs2, imm) {
+    this.rs1 = rs1;
+    this.rs2 = rs2;
+    this.imm = imm;
+    this.toString = function() {
+        if (rs1 == 0) {
+            return `bgez ${IntRegNames[rs2]}, #${imm}`;
+        } else if (rs2 == 0) {
+            return `bgez ${IntRegNames[rs1]}, #${imm}`;
+        } else {
+            return `bge ${IntRegNames[rs1]},${IntRegNames[rs2]},#${imm}`;
+        }
+    }
+}
+
+function BLTU(rs1, rs2, imm) {
+    this.rs1 = rs1;
+    this.rs2 = rs2;
+    this.imm = imm;
+    this.toString = function() {
+        return `bltu ${IntRegNames[rs1]},${IntRegNames[rs2]},${imm}`;
+    }
+}
+
+function BGEU(rs1, rs2, imm) {
+    this.rs1 = rs1;
+    this.rs2 = rs2;
+    this.imm = imm;
+    this.toString = function() {
+        return `bgeu ${IntRegNames[rs1]},${IntRegNames[rs2]},${imm}`;
     }
 }
 
@@ -83,6 +161,18 @@ function decode(insn) {
     const rd = (insn >> 7) & 0x1f;
     const funct3 = (insn >> 12) & 0x7;
     const rs1 = (insn >> 15) & 0x1f;
+    const rs2 = (insn >> 20) & 0x1f;
+
+    const imm_jal = sign_extend(21,
+        pick(insn, 31) << 20 |
+        pick(insn, 21, 10) << 1 |
+        pick(insn, 20) << 11 |
+        pick(insn, 12, 8) << 12);
+    const imm_branch = sign_extend(13,
+        pick(insn, 31) << 12 |
+        pick(insn, 7) << 11 |
+        pick(insn, 25, 6) << 5 |
+        pick(insn, 8, 4) << 1);
 
     switch (opcode) {
     case 0b0110111:
@@ -90,17 +180,29 @@ function decode(insn) {
     case 0b0010111:
         return new AUIPC(rd, insn & 0xfffff000);
     case 0b1101111:
-        return new JAL(rd, sign_extend(21,
-            pick(insn, 31) << 20 |
-            pick(insn, 21, 10) << 1 |
-            pick(insn, 20) << 11 |
-            pick(insn, 12, 8) << 12));
+        return new JAL(rd, imm_jal);
     case 0b1100111:
         if (funct3 == 0b000) {
             return new JALR(rd, rs1, sign_extend(12,
                 pick(insn, 20, 12)));    
         }
         else {
+            return new UnknownOp();
+        }
+    case 0b1100011:
+        if (funct3 == 0b000) {
+            return new BEQ(rs1, rs2, imm_branch);
+        } else if (funct3 == 0b001) {
+            return new BNE(rs1, rs2, imm_branch);
+        } else if (funct3 == 0b100) {
+            return new BLT(rs1, rs2, imm_branch);
+        } else if (funct3 == 0b101) {
+            return new BGE(rs1, rs2, imm_branch);
+        } else if (funct3 == 0b110) {
+            return new BLTU(rs1, rs2, imm_branch);
+        } else if (funct3 == 0b111) {
+            return new BGEU(rs1, rs2, imm_branch);
+        } else {
             return new UnknownOp();
         }
     default:

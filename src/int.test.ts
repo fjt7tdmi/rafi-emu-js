9eaa54fc936412a046757eedff46a6c280ac05d4
Akifumi Fujita <fujita.akifumi@gmail.com>
2020-11-13 00:08:52 +0900

    implement int.ts and test

--- src/int.test.ts
+++ src/int.test.ts
@@ -1,6 +1,20 @@
-import { Int } from "./int";
+import { Int, add, sub } from "./int";
 
 test('initial value', () => {
     const zero = new Int();
-    expect(zero.toInt32()).toBe(0);
+    expect(zero.toNumber()).toBe(0);
+
+    const one = new Int(1);
+    const two = new Int(2);
+    const minusOne = new Int(-1);
+    const minusTwo = new Int(-2);
+
+    expect(add(one, two).toNumber()).toBe(3);
+    expect(add(one, minusOne).toNumber()).toBe(0);
+    expect(add(two, minusOne).toNumber()).toBe(1);
+    expect(add(one, minusTwo).toNumber()).toBe(-1);
+    expect(add(minusOne, minusTwo).toNumber()).toBe(-3);
+
+    expect(sub(two, one).toNumber()).toBe(1);
+    expect(sub(one, two).toNumber()).toBe(-1);
 });
--- src/int.ts
+++ src/int.ts
@@ -1,11 +1,19 @@
 export class Int {
-    public value: number
+    private value: number = 0;
 
-    constructor() {
-        this.value = 0;
+    constructor(value: number = 0) {
+        this.value = value & 0xffffffff;
     }
 
-    toInt32() {
-        return this.value & 0xffffffff;
+    toNumber() {
+        return this.value;
     }
+}
+
+export function add(x: Int, y: Int) {
+    return new Int(x.toNumber() + y.toNumber())
+}
+
+export function sub(x: Int, y: Int) {
+    return new Int(x.toNumber() - y.toNumber())
 }
